2e0b5cd59bb14ee8013a68235153fabdffc94387
sbluiz <sbluizcouto@gmail.com>
2025-11-05 14:05:06 -0300

    Styling the login screen, creating a navbar.

--- src/components/ui/FormLogin.jsx
+++ src/components/ui/FormLogin.jsx
@@ -6,31 +6,33 @@ import { Input } from "./input.jsx"
 export function FormLogin() {
     return (
         <>
-            <div className="flex items-center justify-center h-screen bg-gray-10 p-4 space-y-6">
+            <div className="flex items-center justify-center bg-gray-10 p-4 space-y-6 gap-15" >
 
-                <div className="h-[350px]">
-                    <h1 className="text-2xl pt-5">Bem-vindo ao React & Shadcn!</h1>
+                <div className="space-y-5">
+                    <h1 className="text-5xl pt-5 text-blue-600 font-bold">Bem-vindo ao React & Shadcn!</h1>
                     <p className="w-[290px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, fuga. Dolorum, obcaecati quasi natus cum ratione quam corporis</p>
                 </div>
                 
-                <Tabs defaultValue="account" className="w-[350px] h-[350px] border border-black rounded-lg p-5">
-                    <TabsList className="w-full">
-                        <TabsTrigger value="loginaccount">Fazer Login</TabsTrigger>
-                        <TabsTrigger value="createaccount">Criar Conta</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="loginaccount" className="space-y-4 ">
-                        <p>Preencha com seu email e senha</p>
-                        <Input placeholder="email" />
-                        <Input placeholder="Senha" />
-                        <Button>Logar</Button>
-                    </TabsContent>
-                    <TabsContent value="createaccount" className="space-y-4 ">
-                        <p>Crie sua conta</p>
-                        <Input placeholder="E-mail" />
-                        <Input placeholder="Senha" />
-                        <Button className={"cente"}>Criar Conta</Button>
-                    </TabsContent>
-                </Tabs>
+                <div>
+                    <Tabs defaultValue="loginaccount" className="w-[350px] bg-blue-600 border border-black rounded-lg p-5">
+                        <TabsList className="w-full">
+                            <TabsTrigger value="loginaccount" className="hover:bg-blue-400">Fazer Login</TabsTrigger>
+                            <TabsTrigger value="createaccount" className="hover:bg-blue-400">Criar Conta</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="loginaccount" className="space-y-4 ">
+                            <p>Preencha com seu email e senha</p>
+                            <Input placeholder="email" className="border-2 border-white p2 rounded-2x font-bold bg-white" />
+                            <Input placeholder="Senha"  className="border-2 border-white p2 rounded-2x font-bold bg-white" />
+                            <Button>Logar</Button>
+                        </TabsContent>
+                        <TabsContent value="createaccount" className="space-y-4 ">
+                            <p>Crie sua conta</p>
+                            <Input placeholder="E-mail" className="border-2 border-white p2 rounded-2x font-bold bg-white"  />
+                            <Input placeholder="Senha" className="border-2 border-white p2 rounded-2x font-bold bg-white"  />
+                            <Button className={"cente"}>Criar Conta</Button>
+                        </TabsContent>
+                    </Tabs>
+                </div>
             </div>
         </>
     )
--- src/screens/SigIn.jsx
+++ src/screens/SigIn.jsx
@@ -1,9 +1,11 @@
 import { FormLogin } from "../components/ui/FormLogin.jsx"
+import { Header } from "../components/ui/Header.jsx"
 
 export function SigIn() {
   return (
     <>
     <div className="w-2xl h-full mx-auto">
+      <Header />
       <FormLogin />
     </div>
     </>
