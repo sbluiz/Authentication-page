2f330a0504d49fbf534995e8da1198b29e5f05c3
sbluiz <sbluizcouto@gmail.com>
2025-11-06 16:35:02 -0300

    .

--- frontend/src/screens/SigIn.jsx
+++ frontend/src/screens/SigIn.jsx
@@ -7,7 +7,7 @@ export function SigIn() {
 
 
 
-      <div className="flex">
+      <div className="flex-col">
         <div>
           <div className="min-h-screen flex items-center justify-center">
             <FormLogin />
